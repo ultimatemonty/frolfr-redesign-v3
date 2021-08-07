3f583465123fee1effef62d75817d11124b8d169
Chris McCuller <chris.mcculler@gmail.com>
2021-08-07 11:58:18 -0400

    upgrade to tailwind v2

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,42 @@
 'use strict';
 
-const tailwindcss = require('tailwindcss')
-
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const isProduction = EmberApp.env() === 'production';
+const autoprefixer = require('autoprefixer');
+const postcssimport = require('postcss-import');
+const tailwind = require('tailwindcss');
 
-module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {});
-
+module.exports = function(defaults) {
+  let app = new EmberApp(defaults, {
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: postcssimport,
+            options: {
+              path: ['node_modules']
+            }
+          },
+          {
+            module: tailwind,
+            options: {
+              config: './app/tailwind.config.js'
+            }
+          }
+        ]
+      },
+      filter: {
+        enabled: true,
+        plugins: [
+          {
+            module: autoprefixer,
+            options: {
+              browsers: ['last 2 versions']
+            }
+          }
+        ]
+      }
+    }
+  });
   return app.toTree();
 };
