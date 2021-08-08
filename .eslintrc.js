e1ebf71a38823509291a91e1e38b57e7df2bf48d
Chris McCuller <chris.mcculler@gmail.com>
2021-08-08 15:30:31 -0400

    :rotating_light: ignore a bunch of node linter warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
         'lib/*/index.js',
         'server/**/*.js',
         'app/styles/tailwind.js',
+        'app/tailwind.config.js',
       ],
       parserOptions: {
         sourceType: 'script',
@@ -51,6 +52,9 @@ module.exports = {
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
         'node/no-unpublished-require': 'off',
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        'no-redeclare': 'off',
       },
     },
     {
@@ -58,5 +62,12 @@ module.exports = {
       files: ['tests/**/*-test.{js,ts}'],
       extends: ['plugin:qunit/recommended'],
     },
+    {
+      // mirage
+      files: ['mirage/**/*.js'],
+      rules: {
+        'no-unused-vars': 'off',
+      }
+    }
   ],
 };
